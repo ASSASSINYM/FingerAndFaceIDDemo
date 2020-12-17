37a2df1f7aa635e4955042009e8eedce4af04745
ASSASSINYM <darlingiloveu@sina.com>
2020-12-17 20:30:03 +0800

    Initial Commit

---
+++ FingerAndFaceIDDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FingerAndFaceIDDemo
+//
+//  Created by ASSASSIN on 2020/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
